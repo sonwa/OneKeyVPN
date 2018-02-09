aa23e36fc93dd0f273107bd241f5707b620ef376
SonWa <six@sonwa.cn>
2018-02-09 15:38:57 +0800

    update

--- vpnsetup_centos.sh
+++ vpnsetup_centos.sh
@@ -451,7 +451,7 @@ service fail2ban restart 2>/dev/null
 service ipsec restart 2>/dev/null
 service xl2tpd restart 2>/dev/null
 
-/bin/rm -rf "/opt/src/vpnsetu*"
+/bin/rm -rf /opt/src/vpnsetu*
 
 cat <<EOF
 
